0b16909241e6158851f3369d552a5990c8236fc4
simeiro <shitimi1633@gmail.com>
2023-08-26 13:41:30 +0900

    fix: アイコンのpadding訂正
    デバイスに依らずにpaddingされるように訂正しました

--- lib/decision_gotcha_page.dart
+++ lib/decision_gotcha_page.dart
@@ -5,96 +5,97 @@ class DecisionGotchaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: const Text("確定ガチャ"),
       ),
-
-      body: Container(
-        // mapアイコン
-        margin: const EdgeInsets.only(top: 630),
-        padding: const EdgeInsets.only(top: 0, right: 0, bottom: 0, left: 0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 画像を均等に配置
+      body: Column(
           children: [
-            Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                image: const DecorationImage(
-                  image: AssetImage(
-                    "images/map.png",
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(8.0),
-                  onTap: () {
-                    // 押下時の処理を記述
-                    Navigator.of(context).pushNamed("/map_page");
-                  },
-                ),
-              ),
+            const Text('decision_gorchaの画面'),
+            const Text('その１'),
+            const Text('その２'),
+            Expanded(
+              child: Container(), // 空のコンテナでスペースを占有
             ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(size.height * 0.01),
+              color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: size.width * 0.15,
+                    height: size.width * 0.15,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: const DecorationImage(
+                        image: AssetImage("images/map.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(8.0),
+                        onTap: () {
+                          Navigator.of(context).pushNamed("/map_page");
+                        },
+                      ),
+                    ),
+                  ),
 
-            Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                image: const DecorationImage(
-                  image: AssetImage(
-                    "images/capsule.png",
+                  Container(
+                    width: size.width * 0.15,
+                    height: size.width * 0.15,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: const DecorationImage(
+                        image: AssetImage("images/capsule.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(8.0),
+                        onTap: () {
+                          Navigator.of(context).pushNamed("/dark_gotcha_page");
+                        },
+                      ),
+                    ),
                   ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(8.0),
-                  onTap: () {
-                    // 押下時の処理を記述
-                    Navigator.of(context).pushNamed("/dark_gotcha_page");
-                  },
-                ),
-              ),
-            ),
 
-            Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                image: const DecorationImage(
-                  image: AssetImage(
-                    "images/bars.png",
+                  Container(
+                    width: size.width * 0.15,
+                    height: size.width * 0.15,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: const DecorationImage(
+                        image: AssetImage("images/bars.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(8.0),
+                        onTap: () {
+                          Navigator.of(context).pushNamed("/setting_page");
+                        },
+                      ),
+                    ),
                   ),
-                  fit: BoxFit.cover,
-                ),
+                ],
               ),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(8.0),
-                  onTap: () {
-                    // 押下時の処理を記述
-                    Navigator.of(context).pushNamed("/setting_page");
-                  },
-                ),
-              ),
-            ),
+            )
           ],
         ),
-      ),
     );
   }
 
